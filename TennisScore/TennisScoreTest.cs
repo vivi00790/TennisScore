9e77ce6aedd36ec26623276d0ca43bdecaf44ebd
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-12 10:53:33 +0800

    add first test

---
+++ TennisScore/TennisScoreTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisScore
+{
+    [TestClass]
+    public class TennisScoreTest
+    {
+        [TestMethod]
+        public void Score_0_0_should_return_All_Love()
+        {
+            var judge = new Judge {Player1 = 0, Player2 = 0};
+            var score = judge.Score();
+            Assert.AreEqual("all love", score);
+        }
+    }
+}
