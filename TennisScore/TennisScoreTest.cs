8143dd1bea2f7537add75bdf2435e93dd529917d
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-12 11:26:59 +0800

    add 1-0 test

--- TennisScore/TennisScoreTest.cs
+++ TennisScore/TennisScoreTest.cs
@@ -12,5 +12,13 @@ namespace TennisScore
             var score = judge.Score();
             Assert.AreEqual("all love", score);
         }
+
+        [TestMethod]
+        public void Score_1_0_should_return_15_Love()
+        {
+            var judge = new Judge {Player1 = 1, Player2 = 0};
+            var score = judge.Score();
+            Assert.AreEqual("15 love", score);
+        }
     }
 }
