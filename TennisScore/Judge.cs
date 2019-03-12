09ac6d3ec4888df1a2e4885f3d4bcb5490c74e3e
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-12 10:54:02 +0800

    make through 0-0 case

---
+++ TennisScore/Judge.cs
@@ -0,0 +1,17 @@
+﻿namespace TennisScore
+{
+    internal class Judge
+    {
+        public Judge()
+        {
+        }
+
+        public int Player1 { get; set; }
+        public int Player2 { get; set; }
+
+        public string Score()
+        {
+            return "all love";
+        }
+    }
+}
