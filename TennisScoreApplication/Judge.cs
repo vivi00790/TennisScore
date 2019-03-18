06fac87e6e0b7ce5fbaf6da2f319e1357de1b220
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-18 14:44:35 +0800

    add GUI

--- TennisScoreApplication/Judge.cs
+++ TennisScoreApplication/Judge.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace TennisScore
+namespace TennisScoreApplication
 {
     public class Judge
     {
---
+++ TennisScoreApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisScoreApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Judge judge = new Judge();
+                string input = Console.ReadLine();
+
+                if (input != null)
+                {
+                    string[] data = input.Split(' ');
+                    judge.Player1Name = data[0];
+                    judge.Player1Score = Convert.ToInt32(data[1]);
+                    judge.Player2Name = data[2];
+                    judge.Player2Score = Convert.ToInt32(data[3]);
+                    Console.WriteLine(judge.Score());
+                }
+            }
+        }
+    }
+}
